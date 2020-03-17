d9be1a2f88c7df2ccea6833f4760b5e0698d3790
Abdallah <a.awad@awaeljo.com>
2020-03-17 11:17:05 +0200

    3/17/2020

---
+++ App5_1/App5_1/App5_1/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5_1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        List<String> items = new List<String>();
+        public MainPage()
+        {
+            InitializeComponent();
+            items.Add("Red");
+            items.Add("Yellow");
+            items.Add("Green");
+            items.Add("Blue");
+            items.Add("Pink");
+
+            lst.ItemsSource = items;
+        }
+
+        private void lst_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            pg.ProgressTo(.95,10000,Easing.Linear);
+            string ch = lst.SelectedItem as string;
+            if (ch.Equals("Red")) {
+                this.BackgroundColor = Color.Red;
+            }
+            else if (ch.Equals("Yellow")) {
+                this.BackgroundColor = Color.Yellow;
+            }
+            else if (ch.Equals("Green"))
+            {
+                this.BackgroundColor = Color.Green;
+            }
+            else if (ch.Equals("Blue"))
+            {
+                this.BackgroundColor = Color.Blue;
+            }
+            else if (ch.Equals("Yellow"))
+            {
+                this.BackgroundColor = Color.Yellow;
+            }
+            else if (ch.Equals("Pink"))
+            {
+                this.BackgroundColor = Color.Pink;
+            }
+        }
+
+        private void search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var newData = items.Where<string>(
+                x=>x.StartsWith(search.Text)
+                );
+            lst.ItemsSource = newData;
+        }
+    }
+    }
+
